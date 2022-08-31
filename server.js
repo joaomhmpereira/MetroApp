914f64cbc40585af535e82aba10a52e97cc60630
jmpereira13 <joaompereira07@gmail.com>
2022-08-31 19:34:36 +0100

    server changes

--- server.js
+++ server.js
@@ -16,24 +16,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get('/', (req, res) => {
-  console.log("sending index.html");
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
-app.get("/api/hello", (req, res) => {
-    res.status(200).send("Hello World!");
-});
-
 app.get("/api/estadoServico", (req, res) => {
-    console.log("Getting estadoServico");
     //fetch waiting times for the selected line from the API
     var response = axios.get(`${base_url}/estadoLinha/todos`, { headers: { Authorization: `Bearer ${token}`}})
         .then(response => { return response.data.resposta || [] })
         .catch(error => { return error });
 
     response.then(function(result) {
-        console.log("sending: ");
-        console.log(result);
         res.status(200).send(result);
     })
 });
